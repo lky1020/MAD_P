f256d56b31ac215b0990edabc7dffd29b4a7c27e
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-02-17 10:04:39 +0800

    Update Exercise - BMICalculator (save instance)

--- Week_5/Exercise/app/src/main/java/com/example/exercise/BmiCalculatorActivity.kt
+++ Week_5/Exercise/app/src/main/java/com/example/exercise/BmiCalculatorActivity.kt
@@ -7,6 +7,9 @@ import android.widget.EditText
 import android.widget.TextView
 
 class BmiCalculatorActivity : AppCompatActivity() {
+
+    private var bmi = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi_calculator)
@@ -18,26 +21,37 @@ class BmiCalculatorActivity : AppCompatActivity() {
         val tvName = findViewById<TextView>(R.id.tvName)
         tvName.text = name
 
+        val tvWeightStatus = findViewById<TextView>(R.id.tvWeightStatus)
+
+        if(savedInstanceState != null){
+           bmi = savedInstanceState.getDouble("bmi")
+            tvWeightStatus.text = getBMIStatus()
+        }
+
         //Calculate BMI
         val btnCalculate = findViewById<Button>(R.id.btnCalculate)
         val etWeight = findViewById<EditText>(R.id.etWeight)
         val etHeight = findViewById<EditText>(R.id.etHeight)
-        val tvWeightStatus = findViewById<TextView>(R.id.tvWeightStatus)
+
 
         btnCalculate.setOnClickListener {
             //Get the value
             val weight = etWeight.text.toString().toDouble()
             val height = etHeight.text.toString().toDouble()
-
-            val bodyStatus = calculateBMI(weight, height)
+            bmi = weight / (height * height)
 
             //Display status
-            tvWeightStatus.text = bodyStatus
+            tvWeightStatus.text = getBMIStatus()
         }
     }
 
-    private fun calculateBMI(weight: Double, height: Double): String{
-        val bmi = weight / (height * height)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putDouble("bmi", bmi)
+    }
+
+    private fun getBMIStatus(): String{
 
         when {
             bmi < 18.5 -> {
