3d8242d30f6363d39f827260d17b2d323fbd8ac1
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-02-24 18:18:32 +0800

    Update TrackMySleepQuality-Starter (Create Room DB)

--- Week_7/TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ Week_7/TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,39 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase(){
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    //allows clients to access the methods for creating or getting the database without instantiating the class
+    companion object {
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    )
+
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
