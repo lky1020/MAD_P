2de4f65b63743aa5c93b91d8751d35206018b463
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-02-28 08:23:33 +0800

    pdate TrackMySleepQuality-Starter (Button)

--- Week_7/TrackMySleepQuality-Starter/app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
+++ Week_7/TrackMySleepQuality-Starter/app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
@@ -39,35 +39,35 @@ import java.io.IOException
 @RunWith(AndroidJUnit4::class)
 class SleepDatabaseTest {
 
-    private lateinit var sleepDao: SleepDatabaseDao
-    private lateinit var db: SleepDatabase
-
-    //Create db
-    @Before
-    fun createDb() {
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        // Using an in-memory database because the information stored here disappears when the
-        // process is killed.
-        db = Room.inMemoryDatabaseBuilder(context, SleepDatabase::class.java)
-                // Allowing main thread queries, just for testing.
-                .allowMainThreadQueries()
-                .build()
-        sleepDao = db.sleepDatabaseDao
-    }
-
-    //Close Db
-    @After
-    @Throws(IOException::class)
-    fun closeDb() {
-        db.close()
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun insertAndGetNight() {
-        val night = SleepNight()
-        sleepDao.insert(night)
-        val tonight = sleepDao.getTonight()
-        assertEquals(tonight?.sleepQuality, -1)
-    }
+//    private lateinit var sleepDao: SleepDatabaseDao
+//    private lateinit var db: SleepDatabase
+//
+//    //Create db
+//    @Before
+//    fun createDb() {
+//        val context = InstrumentationRegistry.getInstrumentation().targetContext
+//        // Using an in-memory database because the information stored here disappears when the
+//        // process is killed.
+//        db = Room.inMemoryDatabaseBuilder(context, SleepDatabase::class.java)
+//                // Allowing main thread queries, just for testing.
+//                .allowMainThreadQueries()
+//                .build()
+//        sleepDao = db.sleepDatabaseDao
+//    }
+//
+//    //Close Db
+//    @After
+//    @Throws(IOException::class)
+//    fun closeDb() {
+//        db.close()
+//    }
+//
+//    @Test
+//    @Throws(Exception::class)
+//    fun insertAndGetNight() {
+//        val night = SleepNight()
+//        sleepDao.insert(night)
+//        val tonight = sleepDao.getTonight()
+//        assertEquals(tonight?.sleepQuality, -1)
+//    }
 }
